d56264bb81b3f382885992806c311d7914de4606
ilbball <ilbball.lu@gmail.com>
2024-05-14 12:29:18 +0800

    feat:新增gpt-4o

--- bot/openai_utils.py
+++ bot/openai_utils.py
@@ -21,8 +21,8 @@ OPENAI_COMPLETION_OPTIONS = {
 
 
 class ChatGPT:
-    def __init__(self, model="gpt-4-turbo"):
-        assert model in {"text-davinci-003", "gpt-3.5-turbo-16k", "gpt-4-turbo"}, f"Unknown model: {model}"
+    def __init__(self, model="gpt-4o"):
+        assert model in {"text-davinci-003", "gpt-3.5-turbo-16k", "gpt-4o"}, f"Unknown model: {model}"
         self.model = model
 
     async def send_message(self, message, dialog_messages=[], chat_mode="assistant"):
@@ -33,7 +33,7 @@ class ChatGPT:
         answer = None
         while answer is None:
             try:
-                if self.model in {"gpt-3.5-turbo-16k", "gpt-4-turbo"}:
+                if self.model in {"gpt-3.5-turbo-16k", "gpt-4o"}:
                     messages = self._generate_prompt_messages(message, dialog_messages, chat_mode)
                     r = await openai.ChatCompletion.acreate(
                         model=self.model,
@@ -73,7 +73,7 @@ class ChatGPT:
         answer = None
         while answer is None:
             try:
-                if self.model in {"gpt-3.5-turbo-16k", "gpt-4-turbo"}:
+                if self.model in {"gpt-3.5-turbo-16k", "gpt-4o"}:
                     messages = self._generate_prompt_messages(message, dialog_messages, chat_mode)
                     r_gen = await openai.ChatCompletion.acreate(
                         model=self.model,
